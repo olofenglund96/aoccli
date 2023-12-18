d062c7d6c023aae1ffda441180c587716d80babc
Olof Englund <olofenglund96@gmail.com>
2023-12-18 21:36:49 +0100

    Fix string interp when finding day url

--- helpers/urls.go
+++ helpers/urls.go
@@ -2,18 +2,18 @@ package helpers
 
 import "fmt"
 
-func GetDayUrl(domain string, year string, day string) string {
-	return fmt.Sprintf("%s/%d/day/%d", domain, year, day)
+func GetDayUrl(url string, year string, day string) string {
+	return fmt.Sprintf("%s/%s/day/%s", url, year, day)
 }
 
-func GetDayInputUrl(domain string, year string, day string) string {
-	return fmt.Sprintf("%s/input", GetDayUrl(domain, year, day))
+func GetDayInputUrl(url string, year string, day string) string {
+	return fmt.Sprintf("%s/input", GetDayUrl(url, year, day))
 }
 
-func GetDaySubmitUrl(domain string, year string, day string) string {
-	return fmt.Sprintf("%s/answer", GetDayUrl(domain, year, day))
+func GetDaySubmitUrl(url string, year string, day string) string {
+	return fmt.Sprintf("%s/answer", GetDayUrl(url, year, day))
 }
 
-func GetLeaderboardUrl(domain string, leaderboardId string) string {
-	return fmt.Sprintf("%s/leaderboard/private/view/%s.json", domain, leaderboardId)
+func GetLeaderboardUrl(url string, leaderboardId string) string {
+	return fmt.Sprintf("%s/leaderboard/private/view/%s.json", url, leaderboardId)
 }
